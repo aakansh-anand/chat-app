6374ffdc00eb731c4bc9693701bd2085f831262f
Aakansh Anand <aakanshanand15@gmail.com>
2026-02-05 17:42:02 +0530

    Code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,15 +5,17 @@ export const generateToken = async (userId, response) => {
   /* 1. payload
      2. secret key
      3. options    */
-  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+  const { JWT_SECRET, NODE_ENV, JWT_EXPIRES_IN } = process.env;
+  if (!JWT_SECRET) throw new Error("JWT Secret is not configured.");
+  const token = jwt.sign({ userId: userId }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN || "15d",
   });
 
   response.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milli seconds
     httpOnly: true, // This prevents XSS from client side javascript attacks.
     sameSite: "strict", // This prevents CSRF attacks.
-    secure: process.env.NODE_ENV === "development" ? false : true, // This prevents XSS from client side javascript attacks.
+    secure: NODE_ENV === "development" ? false : true, // This prevents XSS from client side javascript attacks.
   });
 
   return token;
