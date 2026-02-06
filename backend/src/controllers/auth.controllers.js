6f03d8091eda0579431f65d14bbbb550fec9d516
Aakansh Anand <aakanshanand15@gmail.com>
2026-02-06 19:13:07 +0530

    Login & Logout feature complete

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -97,9 +97,50 @@ export const signup = async (request, response) => {
 };
 
 export const login = async (request, response) => {
-  response.send("Login Endpoint");
+  const { email, password } = request.body;
+  try {
+    // check if all fields are provided.
+    if (!email || !password) {
+      return response.status(400).json({
+        message: "All field are required",
+      });
+    }
+
+    // check if user already exists
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      // Never tell if email is wrong or password is wrong, just tell that something is wrong
+      return response.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    // Check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      foundUser.password,
+    );
+    if (!isPasswordCorrect)
+      return response.status(400).json({
+        message: "Invalid Credentials",
+      });
+
+    generateToken(foundUser._id, response);
+    response.status(200).json({
+      _id: foundUser._id,
+      fullName: foundUser.fullName,
+      email: foundUser.email,
+      profilePic: foundUser.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller", error);
+    response.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
-export const logout = async (request, response) => {
-  response.send("Logout Endpoint");
+export const logout = (_, response) => {
+  response.cookie("jwt", "", { maxAge: 0 });
+  response.status(200).json({ message: "User logged out successfully" });
 };
